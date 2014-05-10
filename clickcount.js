f5f9056ef413dfa5059607dfcb465529ddb7485c
Conner Swann <me@connerswann.me>
2014-05-09 21:31:31 -0700

    First commit, the application counts all the clicks any user clicks and puts an object in the db on each click.

---
+++ clickcount.js
@@ -0,0 +1,38 @@
+Clicks = new Meteor.Collection("clicks");
+
+if (Meteor.isClient) {
+	Template.click.nClicks = function(){
+		Meteor.call("clicks");
+		return Session.get("clicks");
+	};
+	Template.click.isWinner = function(){
+		Meteor.call("clicks")
+		clicks = Session.get("clicks");
+		if(clicks%5 === 0)
+			return true;
+		else
+			return false;
+	}
+	Template.click.events({
+		'click input': function () {
+			// template data, if any, is available in 'this'
+			time = new Date().getTime()
+			newClick = {
+				time: time,
+			}
+			var id = Clicks.insert(newClick);
+			if (typeof console !== 'undefined')
+				console.log("Just inserted '" + id + "'into Clicks");
+		}
+	});
+}
+
+Meteor.methods({
+	clicks: function(){
+		Session.set("clicks", Clicks.find().count());
+	}
+});
+
+if (Meteor.isServer) {
+	//Stuff will go here eventually
+};
